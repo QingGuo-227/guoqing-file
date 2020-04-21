c1473e69d96bfccecb563cea9b122f4460e5cc40
qingGuo <qingGuo@example.com>
2020-04-21 11:01:09 +0800

    'guoqing_file'

---
+++ components/dior.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>my apple</h1>
+        <h3>dior and ysl</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
